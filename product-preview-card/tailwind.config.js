38cd7c88fc4050c39188d764f48ba93d1cbe6c8b
Florent Vandry <florentvandroy@gmail.com>
2022-08-06 16:43:45 +0200

    Finished project

---
+++ product-preview-card/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      "white": "#ffffff"
+    },
+
+    extend: {
+      colors: {
+        "dark-cyan": "#3c8067",
+        "cream": "#f2ebe3",
+
+        // Colors Neutral
+        "very-dark-blue": "#1c232b",
+        "dark-grayish-blue": "#6c7289",
+      }
+    },
+  },
+  plugins: [],
+}
