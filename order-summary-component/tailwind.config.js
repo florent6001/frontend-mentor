b24208e9f0f8502a1d66323a3a3701a9918ed151
Florent Vandry <florentvandroy@gmail.com>
2022-08-07 18:50:07 +0200

    Order summary component

---
+++ order-summary-component/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "pale-blue": "#E0E8FF",
+        "light-grey": "#F8F9FE",
+        "dark-blue": "#382AE1",
+        "pale-blue": "#766CF1"
+      }
+    },
+  },
+  plugins: [],
+}
