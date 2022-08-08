c7be86c02d1b0bf39f6eef9df9a1728acb7397e2
Florent Vandry <florentvandroy@gmail.com>
2022-08-08 20:18:04 +0200

    Add social proof section

---
+++ social-proof-section/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "violet": "#511F50",
+        "gray": "#F4F4F4",
+        "light-violet": "#EE68A4",
+        "rating-box": "#F5F3F6"
+      }
+    },
+  },
+  plugins: [],
+}
