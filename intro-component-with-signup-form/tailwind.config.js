1de356dd37c2c47bf02b33492d7651f6107e92cd
Florent Vandroy <florentvandroy@gmail.com>
2022-08-09 21:35:19 +0000

    Add Intro component with signup form

---
+++ intro-component-with-signup-form/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "red": "#ff7a7a",
+        "blue": "#6055a5",
+        "green": "#38cc8c"
+      }
+    },
+  },
+  plugins: [],
+}
