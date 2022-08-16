71c379b7c4c4b647b8d8dc51f4c21777f5a37bf2
Florent Vandroy <florentvandroy@gmail.com>
2022-08-16 12:21:55 +0000

    Add hover effect

--- junior/expenses-chart-component/app.js
+++ junior/expenses-chart-component/app.js
@@ -13,24 +13,30 @@ var chartContainer = document.querySelector('#chart-container')
 data.then(data => {
     data.forEach(element => {
         var div = document.createElement("div")
-        div.classList.add('flex', 'flex-col', 'mx-1', 'w-full')
+        div.classList.add('flex', 'flex-col', 'w-full')
+
+        // Make the hoverable amount
+        var amount = document.createElement("div")
+        amount.innerHTML = `$${element.amount}`
+        amount.classList.add('bg-dark-brown', 'text-white', 'rounded', 'p-2', 'mb-2', 'font-bold', 'text-xs')
+        amount.id = 'hover-amount'
+        div.appendChild(amount)
         
         // Make the bar chart
         var bar = document.createElement("div")
         const height = element.amount / 7
-        bar.classList.add('h-full', 'rounded')
+        bar.classList.add('rounded', 'mx-2')
         if(element.day == 'wed')
             bar.classList.add('bg-cyan')
         else
             bar.classList.add('bg-soft-red')
         bar.style.height = `${height}em`
-        bar.innerHTML = '&nbsp;'
         div.appendChild(bar)
 
         // Add the text below the bar
         var bartext = document.createElement("div")
         bartext.innerHTML = element.day
-        bartext.classList.add('text-center', 'text-medium-brown', 'text-sm')
+        bartext.classList.add('text-center', 'text-medium-brown', 'text-sm', 'w-full')
         div.appendChild(bartext)
         
         chartContainer.appendChild(div);
--- junior/expenses-chart-component/tailwind.config.js
+++ junior/expenses-chart-component/tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         "soft-red": "hsl(10, 79%, 65%)",
         "cream": "hsl(27, 66%, 92%)",
         "medium-brown": "hsl(28, 10%, 53%)",
-        "cyan": "hsl(186, 34%, 60%)"
+        "cyan": "hsl(186, 34%, 60%)",
+        "dark-brown": "hsl(25, 47%, 15%)"
       }
     },
   },
