0c705f5cd8ad45446cc52ecd5ffd105ff9501339
Florent Vandroy <florentvandroy@gmail.com>
2022-08-17 11:35:59 +0000

    Try on mobile

--- junior/intro-section-with-dropdown-navigation/tailwind.config.js
+++ junior/intro-section-with-dropdown-navigation/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       colors: {
         "almost-white": "hsl(0, 0%, 98%)",
         "medium-gray": "hsl(0, 0%, 41%)",
-        "almost-black": "hsl(0, 0%, 8%)"
+        "almost-black": "hsl(0, 0%, 8%)",
+        "behind-nav-black": "rgba(0, 0, 0, 0.5)"
       }
     },
   },
