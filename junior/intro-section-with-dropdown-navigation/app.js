a5fa69f6b96d33ff0ffd8a7edfda07d045f36991
Florent Vandroy <florentvandroy@gmail.com>
2022-08-17 12:17:07 +0000

    Add navbar toggler and dropdown toggler

---
+++ junior/intro-section-with-dropdown-navigation/app.js
@@ -0,0 +1,26 @@
+dropdown_togglers = document.getElementsByClassName('dropdown-toggler')
+
+var toggleNavbar = function () {
+    behind_navbar = document.getElementById('behind_mobile_nav');
+    navbar = document.getElementById('mobile_nav');
+
+    if(navbar.classList.contains('hidden')) {
+        navbar.classList.add('bg-behind-nav-black', 'flex', 'w-full', 'h-full', 'fixed', 'top-0', 'right-0')
+        navbar.classList.remove('hidden')
+    } else {
+        behind_navbar.classList.remove('bg-behind-nav-black', 'flex', 'w-full', 'h-full', 'fixed', 'top-0', 'right-0')
+        navbar.classList.add('hidden')
+    }
+}
+
+var toggleDropdown = function() {
+    target = document.getElementById(this.getAttribute('data-dropdown'));
+    if(target.classList.contains('active'))
+        target.classList.remove('active')
+    else
+        target.classList.add('active')
+}
+
+for (var i = 0; i < dropdown_togglers.length; i++) {
+    dropdown_togglers[i].addEventListener('click', toggleDropdown, false);
+}
