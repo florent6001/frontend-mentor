6c7cafaff88351ccf31a7b4b6500c07e1c0d6201
Florent Vandroy <florentvandroy@gmail.com>
2022-08-23 12:56:06 +0000

    Time tracking challenge done

--- junior/time-tracking-dashboard/app.js
+++ junior/time-tracking-dashboard/app.js
@@ -1,11 +1,35 @@
-async function getData() {
+async function getData(timeframe) {
+
     let data = []
     
     await fetch('data.json')
             .then((res) => res.json())
             .then((res) => data = res)
 
-    console.log(data)
+    data.map((element) => {
+        let current_time = document.getElementById(element.title.replace(/\s/g, '-').toLowerCase() + '-current')
+        let previous_time = document.getElementById(element.title.replace(/\s/g, '-').toLowerCase() + '-previous')
+        let last_label;
+
+        switch(timeframe) {
+            case 'daily':
+                last_label = 'Day'
+                break;
+            case 'hourly':
+                last_label = 'Hour'
+                break;
+            default: 
+                last_label = 'Week'
+                break;
+        }
+
+        current_time.innerHTML = element.timeframes[timeframe].current + 'hrs';
+        previous_time.innerHTML = 'Last ' + last_label + ' - ' + element.timeframes[timeframe].previous + 'hrs';
+        if(document.getElementsByClassName('active').length) {
+            document.getElementsByClassName('active')[0].classList.remove('active');
+        }
+        document.getElementById(timeframe).classList.add('active');
+    })
 }
 
-getData()
+getData('weekly')
